2e23ef6ad4826edd9de385d7c3109fd3e0b365f7
Raj Tilak <machyuthraj@gmail.com>
2024-04-08 16:25:14 +0530

    Code for Abstraction

---
+++ src/DesingPatters/OOPs/Abstraction.java
@@ -0,0 +1,31 @@
+package DesingPatters.OOPs;
+
+public class Abstraction {
+    private int age;
+    private double salary;
+    private String location;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
--- src/DesingPatters/OOPs/Main.java
+++ src/DesingPatters/OOPs/Main.java
@@ -5,5 +5,12 @@ public class Main {
         Encapsulation enc = new Encapsulation();
         enc.setName("ABC");
         System.out.println("Name of my company is " + enc.getName());
+        Abstraction abs = new Abstraction();
+        abs.setAge(26);
+        abs.setSalary(777777.77);
+        abs.setLocation("Washington DC");
+        System.out.println("Age is " + abs.getAge());
+        System.out.println("Salary is " + abs.getSalary());
+        System.out.println("Location is " + abs.getLocation());
     }
 }
