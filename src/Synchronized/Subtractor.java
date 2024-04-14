69cf6164d772844cffe6ab676452046cfaf57de0
Raj Tilak <machyuthraj@gmail.com>
2024-04-14 20:11:56 +0530

    Synchronized keyword is used

---
+++ src/Synchronized/Subtractor.java
@@ -0,0 +1,20 @@
+package Synchronized;
+
+import java.util.concurrent.locks.Lock;
+
+public class Subtractor implements Runnable{
+    Count count;
+
+    public Subtractor(Count count) {
+        this.count = count;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100000; i++) {
+            synchronized (count) {
+                count.value -= 1;
+            }
+        }
+    }
+}
---
+++ src/Synchronized/main.java
@@ -0,0 +1,25 @@
+package Synchronized;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class main {
+    public static void main(String[] args) throws InterruptedException {
+        Count count = new Count();
+
+        Adder adder = new Adder(count);
+        Subtractor subtractor = new Subtractor(count);
+
+        Thread thread = new Thread(adder);
+        Thread thread1 = new Thread(subtractor);
+
+        thread.start();
+        thread1.start();
+
+        thread.join();
+        thread1.join();
+
+        System.out.println(count.value);
+    }
+
+}
